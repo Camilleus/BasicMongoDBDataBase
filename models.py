c7ea8cd85f43c403de3f886e2c10f55a31b7d897
CamilleusRex <camilleusrex@gmail.com>
2024-01-03 09:32:23 +0100

    added Contact class

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from mongoengine import Document, StringField, DateTimeField, ListField, ReferenceField
+from mongoengine import Document, StringField, DateTimeField, ListField, ReferenceField, BooleanField
 
 class Author(Document):
     fullname = StringField(required=True)
@@ -10,3 +10,10 @@ class Quote(Document):
     tags = ListField(StringField())
     author = ReferenceField(Author)
     quote = StringField()
+
+class Contact(Document):
+    fullname = StringField(required=True)
+    email = StringField(required=True)
+    sent_email = BooleanField(default=False)
+    phone_number = StringField()
+    preferred_method = StringField(choices=["email", "sms"])
