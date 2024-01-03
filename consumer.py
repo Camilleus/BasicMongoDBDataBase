957b1e64925799af01a812083080c4e944dc7eaf
CamilleusRex <camilleusrex@gmail.com>
2024-01-03 10:26:47 +0100

    added send_email function

--- consumer.py
+++ consumer.py
@@ -2,4 +2,12 @@ import pika
 from mongoengine import connect
 from models import Contact
 
+
 connect("your_mongodb_uri")
+
+
+def send_email(contact_id):
+    contact = Contact.objects.get(id=contact_id)
+    print(f"Sending email to {contact.fullname} at {contact.email}")
+    contact.sent_email = True
+    contact.save()
