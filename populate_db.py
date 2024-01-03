2b1493d55a882755f27f01c4c5c80fa373f4074d
CamilleusRex <camilleusrex@gmail.com>
2024-01-03 09:36:24 +0100

    added saving data to quotes

--- populate_db.py
+++ populate_db.py
@@ -13,3 +13,10 @@ with open("quotes.json", "r") as f:
 for author_data in authors_data:
     author = Author(**author_data)
     author.save()
+    
+for quote_data in quotes_data:
+    author_fullname = quote_data["author"]
+    author = Author.objects(fullname=author_fullname).first()
+    quote_data["author"] = author
+    quote = Quote(**quote_data)
+    quote.save()
