a48bf075c01a1a7c61e16db511ced59d503e4f8e
CamilleusRex <camilleusrex@gmail.com>
2024-01-03 10:14:33 +0100

    added fake contact generator

--- producer.py
+++ producer.py
@@ -4,4 +4,20 @@ from models import Contact
 import json
 import faker
 
+
 connect("your_mongodb_uri")
+
+
+def generate_fake_contacts(num_contacts):
+    fake = faker.Faker()
+    contacts = []
+    for _ in range(num_contacts):
+        contact = {
+            "fullname": fake.name(),
+            "email": fake.email(),
+            "sent_email": False,
+            "phone_number": fake.phone_number(),
+            "preferred_method": fake.random_element(["email", "sms"])
+        }
+        contacts.append(contact)
+    return contacts
