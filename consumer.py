563d71adfbb21bd85d1689372a30c98c88130c2e
CamilleusRex <camilleusrex@gmail.com>
2024-01-03 10:46:56 +0100

    added callback function

--- consumer.py
+++ consumer.py
@@ -1,6 +1,7 @@
 import pika
 from mongoengine import connect
 from models import Contact
+import json
 
 
 connect("your_mongodb_uri")
@@ -13,9 +14,22 @@ def send_email(contact_id):
     contact.save()
 
 
+def callback(ch, method, properties, body):
+    message = json.loads(body)
+    contact_id = message.get("contact_id")
+    if contact_id:
+        send_email(contact_id)
+
+
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
+
 channel.queue_declare(queue='contacts_queue')
 
+
 channel.basic_consume(queue='contacts_queue', on_message_callback=callback, auto_ack=True)
+
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+channel.start_consuming()
