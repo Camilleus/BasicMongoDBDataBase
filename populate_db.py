7dae24c988d73121fb16d233984ffea3043aec2e
CamilleusRex <camilleusrex@gmail.com>
2024-01-03 09:34:01 +0100

    added loading files from json

--- populate_db.py
+++ populate_db.py
@@ -1,3 +1,11 @@
 from mongoengine import connect
 from models import Author, Quote
 import json
+
+connect("your_mongodb_uri")
+
+with open("authors.json", "r") as f:
+    authors_data = json.load(f)
+
+with open("quotes.json", "r") as f:
+    quotes_data = json.load(f)
