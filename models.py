3e5de63697a063094c4825a4cbf8105d87dca9a9
CamilleusRex <camilleusrex@gmail.com>
2024-01-03 15:14:04 +0100

    added verification of email and birth date

--- models.py
+++ models.py
@@ -1,10 +1,17 @@
-from mongoengine import Document, StringField, DateTimeField, ListField, ReferenceField, BooleanField
+from datetime import datetime
+from mongoengine import Document, StringField, DateTimeField, ListField, ReferenceField, BooleanField, ValidationError
+import re
 
 class Author(Document):
     fullname = StringField(required=True)
     born_date = DateTimeField()
     born_location = StringField()
     description = StringField()
+    created_at = DateTimeField(default=datetime.utcnow)
+    
+    def clean(self):
+        if self.born_date and self.born_date > datetime.utcnow():
+            raise ValidationError("Born date cannot be in the future.")
 
 class Quote(Document):
     tags = ListField(StringField())
@@ -17,3 +24,11 @@ class Contact(Document):
     sent_email = BooleanField(default=False)
     phone_number = StringField()
     preferred_method = StringField(choices=["email", "sms"])
+    meta = {
+        'indexes': [
+            {'fields': ['email'], 'unique': True},
+        ],
+    }
+    def clean(self):
+        if self.phone_number and not re.match(r'^\+\d{1,3}-\d{1,14}$', self.phone_number):
+            raise ValidationError("Invalid phone number format.")
