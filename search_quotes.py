e73d05d30973dad95cca126de33c17ae9f36cce1
CamilleusRex <camilleusrex@gmail.com>
2024-01-03 09:42:37 +0100

    added search_quotes function

--- search_quotes.py
+++ search_quotes.py
@@ -1,5 +1,21 @@
 from mongoengine import connect
-from models import Author, Quote
+from models import Author, Quote, Contact
 import re
 
 connect("your_mongodb_uri")
+
+def search_quotes(query):
+    if query.startswith("name:"):
+        author_name = query.split(":")[1].strip()
+        authors = Author.objects(fullname__icontains=author_name)
+        quotes = Quote.objects(author__in=authors)
+    elif query.startswith("tag:"):
+        tag_name = query.split(":")[1].strip()
+        quotes = Quote.objects(tags__icontains=tag_name)
+    elif query.startswith("tags:"):
+        tags = query.split(":")[1].strip().split(",")
+        quotes = Quote.objects(tags__in=tags)
+    else:
+        return []
+
+    return quotes
