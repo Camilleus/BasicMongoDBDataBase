9e991806ac068481b3f404d4a59f1e1808fe0e6d
CamilleusRex <camilleusrex@gmail.com>
2024-01-03 15:27:44 +0100

    added basic error handling

--- populate_db.py
+++ populate_db.py
@@ -4,21 +4,21 @@ from mongoengine import connect
 from models import Author, Quote
 import json
 
-connect("mongodb+srv://CamilleusRex:<c47UaZGmGSlIR5PB>@pythonmongodbv1cluster0.na7ldv4.mongodb.net/?retryWrites=true&w=majority")
 
-with open("authors.json", "r") as f:
-    authors_data = json.load(f)
+with connect("mongodb+srv://CamilleusRex:<c47UaZGmGSlIR5PB>@pythonmongodbv1cluster0.na7ldv4.mongodb.net/?retryWrites=true&w=majority"):
+    with open("authors.json", "r") as f:
+        authors_data = json.load(f)
 
-with open("quotes.json", "r") as f:
-    quotes_data = json.load(f)
-    
-for author_data in authors_data:
-    author = Author(**author_data)
-    author.save()
-    
-for quote_data in quotes_data:
-    author_fullname = quote_data["author"]
-    author = Author.objects(fullname=author_fullname).first()
-    quote_data["author"] = author
-    quote = Quote(**quote_data)
-    quote.save()
+    with open("quotes.json", "r") as f:
+        quotes_data = json.load(f)
+        
+    for author_data in authors_data:
+        author = Author(**author_data)
+        author.save()
+        
+    for quote_data in quotes_data:
+        author_fullname = quote_data["author"]
+        author = Author.objects(fullname=author_fullname).first()
+        quote_data["author"] = author
+        quote = Quote(**quote_data)
+        quote.save()
