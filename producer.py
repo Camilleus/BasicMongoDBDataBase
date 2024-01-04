4fe721ca66981725bb9fb8bf97d6b9db0f73d029
CamilleusRex <camilleusrex@gmail.com>
2024-01-04 12:43:06 +0100

    added redis

--- producer.py
+++ producer.py
@@ -3,6 +3,8 @@ import json
 import faker
 from mongoengine import connect
 from models import Contact
+import redis
+
 
 def generate_fake_contacts(num_contacts):
     fake = faker.Faker()
@@ -13,18 +15,33 @@ def generate_fake_contacts(num_contacts):
             "email": fake.email(),
             "sent_email": False,
             "phone_number": fake.phone_number(),
-            "preferred_method": fake.random_element(["email", "sms"])
+            "preferred_method": fake.random_element(["email", "sms"]),
+            "tags": [fake.word(), fake.word()]  # Przykładowe dodanie pola "tags"
         }
         contacts.append(contact)
     return contacts
 
-def send_contacts_to_queue(contacts, channel):
+
+def send_contacts_to_queue(contacts, channel, redis_client):
     for contact in contacts:
         contact_doc = Contact(**contact)
         contact_doc.save()
 
+
         message = {"contact_id": str(contact_doc.id)}
         channel.basic_publish(exchange='', routing_key='contacts_queue', body=json.dumps(message))
+
+
+        name_key = f"name:{contact['fullname']}"
+        if not redis_client.get(name_key):
+            redis_client.setex(name_key, 300, json.dumps(contact))
+
+
+        for tag in contact['tags']:
+            tag_key = f"tag:{tag}"
+            if not redis_client.get(tag_key):
+                redis_client.setex(tag_key, 300, json.dumps(contact))
+
 
 if __name__ == "__main__":
     try:
@@ -32,12 +49,18 @@ if __name__ == "__main__":
             connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
             channel = connection.channel()
 
+
             channel.queue_declare(queue='contacts_queue')
+
 
             fake_contacts = generate_fake_contacts(10)
 
-            send_contacts_to_queue(fake_contacts, channel)
+
+            redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+            send_contacts_to_queue(fake_contacts, channel, redis_client)
+            
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
+        
     finally:
         connection.close()
