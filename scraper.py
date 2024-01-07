e9a142a86788f911f797b71e56221959ff2721d9
CamilleusRex <camilleusrex@gmail.com>
2024-01-07 17:46:15 +0100

    added link to webpage

--- scraper.py
+++ scraper.py
@@ -3,7 +3,9 @@ from bs4 import BeautifulSoup
 import json
 
 def scrape_quotes():
-    pass
+    base_url = "http://quotes.toscrape.com"
+    quotes_url = "/page/1"
+    all_quotes = []
 
 
 def save_to_json():
