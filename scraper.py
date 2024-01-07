1beb33ce22f8d680d0826b7066246a1bf5f690ac
CamilleusRex <camilleusrex@gmail.com>
2024-01-07 17:56:10 +0100

    expanded save_to_json

--- scraper.py
+++ scraper.py
@@ -10,8 +10,9 @@ def scrape_quotes():
 
     return all_quotes
 
-def save_to_json():
-    pass
+def save_to_json(data, filename):
+    with open(filename, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=2)
 
 if __name__ == "__main__":
     main()
